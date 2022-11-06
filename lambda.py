a4910da049825fde9ce9df17c94e03968545eeef
Shahmeer <shahmirkhan519@gmail.com>
2022-11-06 16:15:25 +0500

    added lambda function

---
+++ lambda.py
@@ -0,0 +1,13 @@
+'''
+Single statement, without named function in python is called lambda function, 
+and it is also called as the anonymous function in other programming languages. 
+
+For comprehensive explanation, read python-advance.doc file at github.
+'''
+sum = lambda a,b: a+b
+print(sum(80,90))
+print(sum(12,20))
+
+even = lambda a: a%2==0
+print(even(5))
+print(even(90))
