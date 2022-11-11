cd6e108436555aebc23277d58d848b442f83a975
Shahmeer <shahmirkhan519@gmail.com>
2022-11-11 13:50:40 +0500

    working on Regex

--- regex.py
+++ regex.py
@@ -19,10 +19,13 @@ this file. If not, please write to: legal@sequenx.com, or visit www.sequenx.com'
 
 # print(re.findall(text,"Copyright"))
 
-s = "Welcome t00 GeeksForGeeks"
+s = "Welcome to GeeksForGeeks"
  
 # here x is the match object
-res = re.search(r"\s+?Copyright",text  , flags=re.IGNORECASE)
+# res = re.search(r"{to}",s  , flags=re.IGNORECASE)
+# print(res)
+# # print(res.re)
+# print(res.string)
 
-# print(res.re)
-print(res.string)
+output = re.findall(r'Copyright ([\s\S]*)[\n|\r\n|\r]',text, re.IGNORECASE)
+print(output)
