79b19b971245fef08b4e419ee90838d943e5d90c
meer-khan <shahmirkhan519@gmail.com>
2023-03-10 19:35:21 +0500

    working on basic functions of pandas

--- Pandas/pandasDataFrames.py
+++ Pandas/pandasDataFrames.py
@@ -12,3 +12,22 @@ data = {'Name': ['Alice', 'Bob', 'Charlie', 'Dave', 'Eve'],
 # Converting a dictionary into Pandas Dataframe.
 dataset = pd.DataFrame(data)
 print("1. Type of dataset variable is: ", type(dataset))
+
+# * Printing head() of dataframe, head function will give us some top rows of the dataframe
+
+# * Printing info() of dataframe, info function will give us the information about how many columns we have, 
+# how many rows have null values in those columns, and,
+# what is the datatype of each column
+
+# * Printing shape of dataframe, it is attribute not a function, it gives us the rows and columns count (x,y)
+
+# * Printing describe() of dataframe, this function gives the description about the numeric columns data frame like 
+# 1. count of all non null values 
+# 2. mean of numeric tables
+# 3. standard deviations
+# 4. median
+# 5. min value of column
+# 6. 25% of total values in that column
+# 7. 50% of total values in that column
+# 8. 75% of total values in that column
+# 9. max value of column
