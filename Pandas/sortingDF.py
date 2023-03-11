c118b59ac7c705e8b9b13a5ede0a32c59f411797
meer-khan <shahmirkhan519@gmail.com>
2023-03-11 14:23:30 +0500

    selecting multiple columns

--- Pandas/sortingDF.py
+++ Pandas/sortingDF.py
@@ -11,17 +11,31 @@ dataset = pd.DataFrame(data)
 
 # *Sorting data frame
 print("________________________________________________________________________________________")
-print("\nSorted dataframe:\n ", dataset.sort_values("Salary") )
+print("\nSorted dataframe:\n", dataset.sort_values("Salary") )
 
 # * Sorting in descending order
 print("________________________________________________________________________________________")
-print("\nSorted dataframe:\n ", dataset.sort_values("Salary", ascending = False) )
+print("\nSorted dataframe:\n", dataset.sort_values("Salary", ascending = False) )
 
 #* Sorting on multiple colums 
 print("________________________________________________________________________________________")
-print("\nSorted dataframe:\n ", dataset.sort_values(["City","Salary"], ascending = [False,True]) )
+print("\nSorted dataframe:\n", dataset.sort_values(["City","Salary"], ascending = [False,True]) )
 
 # * Taking column out of the dataframe
 print("________________________________________________________________________________________")
-print("\nColumn from the dataframe:\n ",dataset["Name"])
+print("\nColumn from the dataframe:\n",dataset["Name"])
+
+# *Taking multiple columns
+# ! we cannot take out multiple columns in single list, like below, we need to make it in 2D list
+# print("\nMultiple Columns:\n", dataset["Name","Gender"])
+print("________________________________________________________________________________________")
+print("\nMultiple Columns:\n", dataset[["Name","Gender"]])
+
+# # *Another way to selecting multiple colummns from the dataframe
+# print("________________________________________________________________________________________")
+# chunk = dataset["Name", "Gender"]
+# print("\nAnother way:\n", dataset[chunk])
+
+
+
 
