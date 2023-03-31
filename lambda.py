d009637bf62f72388ade5cdd9031df5ff7e3a44f
meer-khan <shahmirkhan519@gmail.com>
2023-04-01 03:41:23 +0500

    changed lambda function file and added more stuff into it

--- lambda.py
+++ lambda.py
@@ -13,3 +13,18 @@ print(even(5))
 print(even(90))
 
 
+
+# pass keyword does not work in lambda function so we use elipses
+mod = lambda x : x if(x >= 0) else ...
+ 
+print(mod(-1))
+
+a = lambda x: len(x) if (isinstance(x,str)) else None 
+a(5)
+
+# https://www.geeksforgeeks.org/lambda-with-if-but-without-else-in-python/
+
+
+
+
+
