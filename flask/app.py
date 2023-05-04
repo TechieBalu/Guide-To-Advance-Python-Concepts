06dad80a9743709ef1e7794e6a0c3e3de7de2a14
meer-khan <shahmirkhan519@gmail.com>
2023-05-04 17:26:00 +0500

    added dynamic urls

--- flask/app.py
+++ flask/app.py
@@ -11,5 +11,15 @@ def hello():
     return "This is hello"
 
 
+# Building Dynami URL 1st type 
+@app.route("/success/<int:score>")
+def success(score):
+    return "This is pass" + str(score)
+
+
+@app.route("/fail/<int:score>")
+def fail(score):
+    return "This is pass" + str(score)
+
 if __name__ == "__main__":
     app.run(debug=True)
