3d1cf8d58037ff7a852d9b35b2fe6f2a91785bce
Shahmeer <shahmirkhan519@gmail.com>
2023-07-26 18:24:31 +0500

    working on encapsulation

--- oop/class.py
+++ oop/class.py
@@ -2,22 +2,47 @@ class Car:
 
     # This is exactly not the constructor but very close to the constructor, we call it a constructor because it is the first method that is called when 
     # we create the object of a class. (Explre difference between the constructor and __init__ method of python class)
-    
-    def __init__(self) -> None:
-        pass 
+    # using self is convention, we can use any variable here, as we know self is called automatically by the "object" class or we can say by python 
+    # and it is a requirement that we need to give one argument in __init__ method. 
+    # We cannot initilize 2 __init__ methods in python.
+    # we can provide default value of arugment as well as we can provide *args (multiple parameters) and **kwargs (keyword arguments(it will be get as a dictionary)).
+    # we can also provide a default value by just writing 
+    # self.age = 10
+    # into body of __init__ function and not in argument 
+
+    # Encapsulation: 
+    # Java and C++ has public private method to encapsulate means to hide the information from the user to change it, let's say e.g.
+    # all the class variables can be easily accessed and modified by the instance, and we donot want that, 
+    # to encapsulate this information, we make getters and setters, and make all class variables private (majority of them)
+    # by making getters and setter funtions we can get control over what variables can be accessed through getters and what can be modified 
+    # and in the modification, user can give string value to some variable that only operates on string, so we can also handle such information gracefully through setters
+    def __init__(self,speed,color) -> None:
+        print("__INIT__") 
+        self.speed = speed
+        self.color = color
+        self.model = 2023
+        self._b = 20
+        self.__c = 30
+        print(speed, color)
 
 
-ford = Car()
-honda = Car()
-audi = Car()
+ford = Car(200,'red')
+honda = Car(200,'green')
+audi = Car(200,'blue')
 
-ford.speed = 200
-honda.speed = 200
-audi.speed = 200
+# ford.speed = 200
+# honda.speed = 200
+# audi.speed = 200
 
-ford.color = 'red'
-honda.color = 'blue'
-audi.color = 'green'
+# ford.color = 'red'
+# honda.color = 'blue'
+# audi.color = 'green'
 
 print(audi.speed, audi.color)
+print(audi)
+print(audi.model)
+print(audi._b)
+print(audi.__c)
+
+
 
