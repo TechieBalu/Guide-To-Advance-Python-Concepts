60c56cbe050af81289998589107318ca4492d0be
meer-khan <shahmirkhan519@gmail.com>
2023-08-08 03:20:54 +0500

    mro method method resolution order to get the calling order of functions

--- oop/super.py
+++ oop/super.py
@@ -2,10 +2,19 @@ class Parent:
     def __init__(self) -> None:
         print("This is parent class")
 
-class Child(Parent):
+
+class Parent2:
     def __init__(self) -> None:
+        print("This is parent2 class")
+
+class Child(Parent, Parent2):
+    def __init__(self) -> None:
+        # super function is builtin function that return the proxy object that allows you to refer to parent class
         super().__init__()
         print("This is CHILD")
 
 
 ch = Child()
+# method resolution order 
+# this is the method in which order that methods are calling inside child class 
+print(Child.__mro__)
