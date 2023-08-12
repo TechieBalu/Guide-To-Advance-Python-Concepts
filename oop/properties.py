689ac287f6bd63b1f89c7b872544d17c7d3f92de
meer-khan <shahmirkhan519@gmail.com>
2023-08-13 00:57:59 +0500

    @property decorator

--- oop/properties.py
+++ oop/properties.py
@@ -10,10 +10,15 @@ class Salary:
     def salary(self):
         return self.__salary 
     
-    # we use "variablename.setter" decorator to set the salary
+    # we use "variablename.setter" decorator to set the salary, it is same as creating a setter function
     @salary.setter
     def salary(self,salaryValue):
         self.__salary = salaryValue
+
+    
+    @salary.deleter
+    def salary(self):
+        del self.__salary
 
 
 sr = Salary()
