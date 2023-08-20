412df514ac0f9217eb9c5cdd8e53947f71d62e7f
meer-khan <shahmirkhan519@gmail.com>
2023-08-21 01:31:16 +0500

    working on non locals

--- interview/scopes.py
+++ interview/scopes.py
@@ -1,6 +1,10 @@
 import ctypes 
+
+
+
 def fun(): 
     x = 10 
+    print(x)
     id1 = id(x)
     del x
     # print(x)
@@ -10,3 +14,30 @@ a = fun()
 print(a)
 print(id(a))
 print(ctypes.cast(a,ctypes.py_object).value)
+
+
+print("_____________________________________________________")
+
+def fun2():
+    x = 1000
+    print(x)
+    
+
+x = 100
+fun2()
+print(x)
+
+print("_____________________________________________________")
+
+
+def outer_function():
+    y = 20  # y is in the enclosing scope of inner_function
+
+    def inner_function():
+        nonlocal y
+        y += 1
+        print(y)
+
+    inner_function()  # Prints 21
+
+outer_function()
