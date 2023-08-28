769ed2bf28dfc23356bb7fb9cce48752c27855b4
meer-khan <shahmirkhan519@gmail.com>
2023-08-29 00:31:53 +0500

    understanding multiple inheritence

--- oop/multiLevelInheritence.py
+++ oop/multiLevelInheritence.py
@@ -1,6 +1,8 @@
 class A:
-   def __init__(self, a_name):
-       self.a_name = a_name
+
+    cool = 50
+    def __init__(self, a_name):
+        self.a_name = a_name
    
 # Intermediate class
 class B(A):
@@ -13,21 +15,23 @@ class B(A):
 class C(B):
    def __init__(self,c_name, b_name, a_name):
         self.c_name = c_name
-        super().__init__(b_name)
+        # print(super)
+        super().__init__(self, b_name, a_name)
 
        # invoke constructor of class B
-    #    B.__init__(self, b_name, a_name)
+        B.__init__(self, b_name, a_name)
        
    def display_names(self):
        print("A name : ", self.a_name)
        print("B name : ", self.b_name)
        print("C name : ", self.c_name)
 
-class A2(A):
-    def __init__(self, a_name):
-        super().__init__(a_name)
+# class A2(A):
+#     def __init__(self, a_name):
+#         super().__init__(a_name)
 
 #  Driver code
 obj1 = C('child', 'intermediate', 'parent')
 print(obj1.a_name)
+print(obj1.cool)
 obj1.display_names()
