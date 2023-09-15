efc118b4571bb11dad0733cf63810fe0e56a9bb0
meer-khan <shahmirkhan519@gmail.com>
2023-09-16 02:50:31 +0500

    created a worker table in sqlalchemy

--- sqlalchemy CRUD/main.py
+++ sqlalchemy CRUD/main.py
@@ -1,5 +1,19 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
 engine = create_engine("sqlite:///sample.db", echo=True)
 print(engine)
 print(type(engine))
+
+meta= MetaData()
+
+# There are 2 ways to create tables 
+# 1. using declarative_base class 
+# 2. Table class 
+workers = Table(
+    'workers',
+    meta,
+    Column('id', Integer,primary_key = True),
+    Column('name', String)
+)
+
+meta.creat_all(engine)
