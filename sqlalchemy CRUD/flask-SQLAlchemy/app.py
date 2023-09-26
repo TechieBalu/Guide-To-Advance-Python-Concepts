12dd0b74cdc135959e5102980093c288975a3eaa
Shahmeer Khan <shahmirkhan519@gmail.com>
2023-09-26 14:06:28 +0500

    created a database and added table and columns in it

--- sqlalchemy CRUD/flask-SQLAlchemy/app.py
+++ sqlalchemy CRUD/flask-SQLAlchemy/app.py
@@ -1,10 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
 # This line is to reduce the number of warning apperaing on the terminal while running the app. 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -26,7 +27,23 @@ db = SQLAlchemy(app)
 # 
 # create_all does not update tables if they are already in the database. If you change a model’s columns, use a migration library like 
 # Alembic with Flask-Alembic or Flask-Migrate to generate migrations that update the database schema. 
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column (db.String(50))
+    email = db.Column(db.String(100), unique=True)  
+    date_joined = db.Column(db.Date, default = datetime.utcnow)
+    
+    def __repr__(self):
+        return f"<User: {self.email}>"
+
+
+
 with app.app_context():
     db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
