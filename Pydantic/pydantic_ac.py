b841f440becb9e97ed242c600835fae661c4e35f
Shahmeer Khan <shahmirkhan519@gmail.com>
2023-11-30 12:44:46 +0500

    checking beartype optional and typing optional

--- Pydantic/pydantic_ac.py
+++ Pydantic/pydantic_ac.py
@@ -1,16 +1,19 @@
 import json
 from typing import List, Optional
 import pydantic 
-
+from icecream import ic
+# from beartype.typing import Optional, List
 class Book(pydantic.BaseModel):
-    title:str
-    author:str
-    publisher:str
-    price:float
+    title: str
+    author: str
+    publisher: str
+    price: float
     isbn_10: Optional[str]
     isbn_13: Optional[str]
     subtitle: Optional[str]
+    # author2: Optional[Author]
 
+    
     
 
 
@@ -21,13 +24,17 @@ def main() -> None:
     # Read data from a JSON file
     with open("./data.json") as file:
         data = json.load(file)
-        print(data)
+        # print(data)
         # Priting title
-        print(data[0]["title"])
+        # print(data[0]["title"])
 
 
-        # books: List[Book] = [Book(**item) for item in data]
+        books: List[Book] = [Book(**item) for item in data]
         # print(books)
+        ic(type(books))
+        ic(books[0].title)
+        ic(type(books[0]))
+
         # print(books[0])
         # print(books[0].dict(exclude={"price"}))
         # print(books[1].copy())
