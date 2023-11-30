43fa1a08b4fb87d8835b9b9c44c55a6f8d6146ef
Shahmeer Khan <shahmirkhan519@gmail.com>
2023-11-30 13:02:08 +0500

    applied validation on sibn10

--- Pydantic/pydantic_ac.py
+++ Pydantic/pydantic_ac.py
@@ -3,6 +3,15 @@ from typing import List, Optional
 import pydantic 
 from icecream import ic
 # from beartype.typing import Optional, List
+
+
+class ISBN10FormatError(Exception): 
+    def __init__(self, value:str, message:str) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(message)
+
+
 class Book(pydantic.BaseModel):
     title: str
     author: str
@@ -13,7 +22,22 @@ class Book(pydantic.BaseModel):
     subtitle: Optional[str]
     # author2: Optional[Author]
 
-    
+    @pydantic.validator("isbn_10")
+    @classmethod
+    def check_ISBN10(cls, value):
+        chars = [c for c in value if c in "0123456789Xx"]
+        if len(chars) != 10:
+            raise ISBN10FormatError(value=value, message="ISBN10 should be 10 digits")
+        
+        def char_into_int(char:str)->int:
+            if char in "Xx":
+                return 10
+            return int(char)
+        
+        weighted_sum = sum((10-i) * char_into_int(x) for i,x in enumerate(chars))
+        if weighted_sum % 11 != 0:
+            raise ISBN10FormatError(value=value, message="ISBN10 digit sum should be divisiable by 11.")
+        # return value
     
 
 
