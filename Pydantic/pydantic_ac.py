6b0d1defb029d6540e56d3bb0a439a1090fcb177
Shahmeer Khan <shahmirkhan519@gmail.com>
2023-11-30 10:58:31 +0500

    creating basemodels

---
+++ Pydantic/pydantic_ac.py
@@ -0,0 +1,37 @@
+import json
+from typing import List, Optional
+import pydantic 
+
+class Book(pydantic.BaseModel):
+    title:str
+    author:str
+    publisher:str
+    price:float
+    isbn_10: Optional[str]
+    isbn_13: Optional[str]
+    subtitle: Optional[str]
+
+    
+
+
+
+def main() -> None:
+    """Main function."""
+
+    # Read data from a JSON file
+    with open("./data.json") as file:
+        data = json.load(file)
+        print(data)
+        # Priting title
+        print(data[0]["title"])
+
+
+        # books: List[Book] = [Book(**item) for item in data]
+        # print(books)
+        # print(books[0])
+        # print(books[0].dict(exclude={"price"}))
+        # print(books[1].copy())
+
+
+if __name__ == "__main__":
+    main()
