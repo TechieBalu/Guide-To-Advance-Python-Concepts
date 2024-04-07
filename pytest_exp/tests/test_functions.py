7bc5ae2aa7f6a7e59e189ca4dfe16867cf530980
meer-khan <shahmirkhan519@gmail.com>
2024-04-08 03:25:39 +0500

    added more test functions

--- pytest_exp/tests/test_functions.py
+++ pytest_exp/tests/test_functions.py
@@ -1,9 +1,6 @@
-import pathlib
-import sys
-BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
-sys.path.append(str(BASE_DIR))
 from icecream import ic
-from functions.functions_file import add_numbers, divide_numbers
+from functions.functions_file import add_numbers, divide_numbers, divide_numbers_zerodivisionerror
+from classes import shapes
 import pytest
 
 
@@ -26,4 +23,21 @@ def test_divide_by_zero():
 
 def test_divide_by_zero_2():
     with pytest.raises(ZeroDivisionError):
-        divide_numbers(10,0)
+        divide_numbers_zerodivisionerror(10,0)
+    
+    assert divide_numbers_zerodivisionerror(10,10) == 1
+
+# def test_divide_by_zero_3():
+#     with pytest.raises(ZeroDivisionError):
+#         divide_numbers_zerodivisionerror(10,0)
+
+# Testing Rectange class 
+# Pytest Fixtures
+# In conftest.py file for making the fixtures global
+
+def test_rectangle_area(my_rectangle): 
+
+    assert my_rectangle.area() == 10*20
+
+def test_rectangle_perimeter(my_rectangle): 
+    assert my_rectangle.perimeter() == (10 + 20) * 2
